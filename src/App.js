c58f9a88f882cd8557f180e2cbb0ebe4be27ceeb
saransh-dhama <saransh014@gmail.com>
2020-06-06 14:42:53 +0200

    Move data to state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './contacts/listContacts';
-const contacts = [
-	{
-		id: 'karen',
-		name: 'Karen Isgrigg',
-		handle: 'karen_isgrigg',
-		avatarURL: 'http://localhost:5001/karen.jpg',
-	},
-	{
-		id: 'richard',
-		name: 'Richard Kalehoff',
-		handle: 'richardkalehoff',
-		avatarURL: 'http://localhost:5001/richard.jpg',
-	},
-	{
-		id: 'tyler',
-		name: 'Tyler McGinnis',
-		handle: 'tylermcginnis',
-		avatarURL: 'http://localhost:5001/tyler.jpg',
-	},
-];
+
 class App extends Component {
+	state = {
+		contacts: [
+			{
+				id: 'tyler',
+				name: 'Tyler McGinnis',
+				handle: '@tylermcginnis',
+				avatarURL: 'http://localhost:5001/tyler.jpg',
+			},
+			{
+				id: 'karen',
+				name: 'Karen Isgrigg',
+				handle: '@karen_isgrigg',
+				avatarURL: 'http://localhost:5001/karen.jpg',
+			},
+			{
+				id: 'richard',
+				name: 'Richard Kalehoff',
+				handle: '@richardkalehoff',
+				avatarURL: 'http://localhost:5001/richard.jpg',
+			},
+		],
+	};
 	render() {
 		return (
 			<div>
-				<ListContacts contacts={contacts} />
+				<ListContacts contacts={this.state.contacts} />
 			</div>
 		);
 	}
